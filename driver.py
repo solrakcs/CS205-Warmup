93c1373f33eb7271de9f47f33a8e70a32a7bd3ca
solrakcs <carlitoscs99@gmail.com>
2020-02-12 09:02:45 -0500

    Some changes in database file

--- driver.py
+++ driver.py
@@ -1,12 +1,16 @@
 from database import Database
 
 def main():
-    db = Database() 
-    #db.print_all_titles()
-    #db.print_all_companies()
-    db.get_vote_average("Avatar")
-    db.get_revenue("Avatar")
-    db.get_proco_name("Avatar")
-    db.get_proco_founder("Avatar")
+    db = Database()
+    loaded = db.load_data()
+    if loaded == 0:
+        db.print_all_titles()
+        db.print_all_companies()
+        db.get_vote_average("Avatar")
+        db.get_revenue("Avatar")
+        db.get_proco_name("Avatar")
+        db.get_proco_founder("Avatar")
+    else:
+    	print("The database was not loaded properly")
 
 main()
